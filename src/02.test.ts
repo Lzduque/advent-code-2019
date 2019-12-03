7ce2acecc08e76e56e569230dd2b07bf5995be53
Leticia <lzduque@hotmail.com>
2019-12-02 21:38:19 -0500

    second day

---
+++ src/02.test.ts
@@ -0,0 +1,9 @@
+import opcode from './02';
+
+test('opcode', () => {
+  expect(opcode([1, 0, 0, 0, 99])).toStrictEqual([2, 0, 0, 0, 99]);
+  expect(opcode([2, 3, 0, 3, 99])).toStrictEqual([2, 3, 0, 6, 99]);
+  expect(opcode([2, 4, 4, 5, 99, 0])).toStrictEqual([2, 4, 4, 5, 99, 9801]);
+  expect(opcode([1, 1, 1, 4, 99, 5, 6, 0, 99])).toStrictEqual([30, 1, 1, 4, 2, 5, 6, 0, 99]);
+  expect(opcode([1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50])).toStrictEqual([3500, 9, 10, 70, 2, 3, 11, 0, 99, 30, 40, 50]);
+})
--- src/02.ts
+++ src/02.ts
@@ -2,13 +2,76 @@ var fs = require("fs");
 
 function read_file(file_name: string): number[] {
   var text = fs.readFileSync(file_name).toString('utf-8');
-  var textByLine = text.split("\n");
-  var to_number = textByLine.map(text => Number(text))
+  var textByLine = text.split(",");
+  var to_number = textByLine.map(text => Number(text));
   return to_number
 }
 
-function opcode(input: number[]) {
+// const add = (x, y) => x + y
+// const mult = (x, y) => x * y
+// const math = (x, y, f) => f(x, y)
+// math(2, 3, add)
+// math(2, 3, mult)
 
+// function mutate_array(num1: number, num2: number, callback) {
+//   return callback(num1, num2);
+// }
+
+function opcode(input: number[]) {
+  let new_array = input;
+  for (let i = 0; i < new_array.length; i += 4) {
+    const num1 = new_array[new_array[i + 1]];
+    const num2 = new_array[new_array[i + 2]];
+
+    switch (new_array[i]) {
+      case 1: {
+        const result = num1 + num2;
+        new_array[new_array[i + 3]] = result
+        break;
+      }
+      case 2: {
+        const result = num1 * num2;
+        new_array[new_array[i + 3]] = result
+        break;
+      }
+      case 99: {
+        console.log("Program finished running");
+        return new_array;
+      }
+      default:
+        console.log("Error: number not reconized");
+        break;
+    }
+  }
+  return new_array;
+}
+
+function fix_array(input: number[], num1: number, num2: number) {
+  let new_array = [...input];
+  new_array[1] = num1;
+  new_array[2] = num2;
+  return new_array
 }
 
 var input = read_file("./src/input_02.txt");
+var fixed_array = fix_array(input, 12, 2);
+var opcode_array = opcode(fixed_array);
+console.log("At position 0 is: " + opcode_array[0]);
+
+function guess_input(input: number[]) {
+  for (let i = 0; i <= 99; i++) {
+    for (let j = 0; j <= 99; j++) {
+      const fixed_array = fix_array(input, i, j);
+      const opcode_array = opcode(fixed_array);
+      if (opcode_array[0] === 19690720) {
+        console.log("I: " + i + " J: " + j);
+        console.log("100 * noun + verb: " + ((100 * i) + j));
+        return;
+      }
+    }
+  }
+}
+
+guess_input(input);
+
+export default opcode;
